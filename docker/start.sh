9244db19deb47f8e4811372ce17549d221b82e09
MelroyB <melroy.bouwes@outlook.com>
2025-02-07 15:49:41 +0100

    new file:   docker/dockerfile 	new file:   docker/start.sh

---
+++ docker/start.sh
@@ -0,0 +1,23 @@
+  GNU nano 7.2                                                       start.sh
+#!/bin/bash
+
+# Controleer of de REPO_URL variabele is ingesteld
+if [ -z "$REPO_URL" ]; then
+  echo "REPO_URL environment variable is not set. Exiting."
+  exit 1
+fi
+
+# Clone de repository
+git clone $REPO_URL repo
+
+# Ga naar de directory van de repository
+cd repo
+
+# Installeer Python dependencies
+pip install -r requirements.txt
+
+# Start de Flask applicatie
+python app.py
+
+
+
